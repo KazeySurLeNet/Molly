c28581d3e4486cdafda0259c0fac09969fd9fa8e
KazeySurLeNet <35663102+KazeySurLeNet@users.noreply.github.com>
2018-03-26 18:08:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ console.log("Molly> Lancement en cours...")
 
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const token = process.env.TOKEN;
 
 
 bot.on('ready', () => {
-console.log('Molly> Lancé avec succès.')
+console.log(`Molly> Lancé avec succès. Sur ${bot.guilds.size} serveur(s) avec ${bot.users.size} user(s)`)
 setInterval(multigame, 10000)
+bot.user.setUsername("Molly");
 });
 
 bot.on("guildMemberAdd", member  =>{
@@ -17,15 +19,15 @@ bot.on("guildMemberAdd", member  =>{
 
 bot.on("message", message => {
     
-    const args = message.content.slice("dpg!").trim().split(" ");
-    const command = args[0].substring(4);
+    const args = message.content.slice("!").trim().split(" ");
+    const command = args[0].substring(1);
     
 if(args.length > 0) args.shift();
 
     if(command === "help"){
     var help_embed = new Discord.RichEmbed()
     .setTitle("Liste des commands")
-    .setDescription("dpg!help : Affiche la liste des commandes\ndpg!main [scout/gunner/ingeneer/driller]")
+    .setDescription("```!help : Affiche la liste des commandes\n!main [scout/gunner/ingeneer/driller]```")
     .setFooter("Molly", "https://media.discordapp.net/attachments/420172766587781120/420270751975866368/molly.png")
     .setColor('RANDOM');
     message.channel.send(help_embed);
@@ -36,6 +38,10 @@ if(args.length > 0) args.shift();
         let args = message.content.split(" ");
         args.shift();
         
+         if(!args[0]){
+            message.channel.send("Syntax : !main [scout/gunner/ingeneer/driller]")
+            return;
+        }
         if(args[0] === "scout"){
            message.member.addRole(message.guild.roles.find("name", "Scout / Éclaireur").id);
             message.channel.send("Tu as reçus le grade Scout");
@@ -62,7 +68,7 @@ if(args.length > 0) args.shift();
 
     bot.user.setPresence({game: {name: `Dev By KAZOU`}});
 
-    bot.user.setPresence({game : {name : 'dpg!help'}});
+    bot.user.setPresence({game : {name : '!help'}});
     
 }
 bot.login(process.env.TOKEN);
